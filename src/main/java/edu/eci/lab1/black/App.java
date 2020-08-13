a4edca4b98018b78a1b595f34fd7a64b23e3552c
alejovasquero <alejo_704@hotmail.com>
2020-08-13 12:24:37 -0500

    Initial commit

---
+++ src/main/java/edu/eci/lab1/black/App.java
@@ -0,0 +1,13 @@
+package edu.eci.lab1.black;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
